61430f732cc02ed91e9945f536ba35dfa7296720
Nischal Baral <nb06021320@studnent.ku.edu.np>
2022-08-23 14:36:26 +0545

    Firebase and view binding added

--- app/src/main/java/com/example/perfecthours/MainActivity.kt
+++ app/src/main/java/com/example/perfecthours/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.perfecthours
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.perfecthours.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
+
 }
