92dfa5231de0350ca2250876e9dea28544450dd8
Nischal Baral <nb06021320@studnent.ku.edu.np>
2022-08-24 23:16:20 +0545

    Login added

--- app/src/main/java/com/example/perfecthours/MainActivity.kt
+++ app/src/main/java/com/example/perfecthours/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.perfecthours
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.perfecthours.databinding.ActivityMainBinding
-
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+        val user=mAuth.currentUser
+
+        Handler().postDelayed({
+            if(user != null){
+                val loggedinIntent = Intent(this, loggedin::class.java)
+                startActivity(loggedinIntent)
+                finish()
+            }else{
+                val signupIntent=Intent(this, signup::class.java)
+                startActivity(signupIntent)
+                finish()
+            }
+        },2000)
 
     }
 
---
+++ app/src/main/java/com/example/perfecthours/loggedin.kt
@@ -0,0 +1,11 @@
+package com.example.perfecthours
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loggedin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loggedin)
+    }
+}
