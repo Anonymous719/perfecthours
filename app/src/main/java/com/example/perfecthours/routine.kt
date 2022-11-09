2121d1c41d105f0aa54b2bbbec0ef803e1f3f56e
Nischal Baral <nischalbaral267@gmail.com>
2022-11-09 21:05:57 +0545

    Login to be updated

--- app/src/main/java/com/example/perfecthours/routine.kt
+++ app/src/main/java/com/example/perfecthours/routine.kt
@@ -12,75 +12,19 @@ import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
-class routine : Fragment(),CalendarAdapter.OnItemListener {
+class routine : Fragment() {
 
     companion object {
         fun newInstance() = Fragment()
     }
 
     private var fragbinding : FragmentRoutineBinding? = null
-    private var selectedDate : LocalDate? = null
-    private lateinit var daysInMonth: ArrayList<String>
-    private lateinit var calendarAdapter: CalendarAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragbinding = FragmentRoutineBinding.inflate(inflater, container, false)
-        selectedDate = LocalDate.now()
-        setMonthView()
-
-        fragbinding!!.prevmonth.setOnClickListener(){
-            selectedDate = selectedDate?.minusMonths(1)
-            setMonthView()
-        }
-        fragbinding!!.nextmonth.setOnClickListener(){
-            selectedDate = selectedDate?.plusMonths(1)
-            setMonthView()
-        }
-
         return fragbinding!!.root
     }
-
-    fun setMonthView(){
-        fragbinding?.yymm?.setText(monthYearFromDate(selectedDate))
-        daysInMonth = daysInMonthArray (selectedDate)
-
-        calendarAdapter = CalendarAdapter(daysInMonth, this)
-
-        var layoutManager:RecyclerView.LayoutManager = GridLayoutManager(this.context, 7)
-        fragbinding!!.calendar.setLayoutManager(layoutManager)
-        fragbinding!!.calendar.setAdapter(calendarAdapter)
-    }
-
-    fun daysInMonthArray(date: LocalDate?):ArrayList<String>{
-        var daysInMonthArray= ArrayList<String>()
-        var yearMonth: YearMonth = YearMonth.from(date)
-
-        var daysInMonth = yearMonth.lengthOfMonth()
-
-        var firstOfMonth : LocalDate = selectedDate!!.withDayOfMonth(1)
-        var dayOfWeek = firstOfMonth.dayOfWeek.value
-
-        for (i in 1..42){
-            if (i<dayOfWeek || i>daysInMonth+dayOfWeek){
-                daysInMonthArray.add("")
-            }else{
-                var temp = i-dayOfWeek
-                daysInMonthArray.add(temp.toString())
-            }
-        }
-        return daysInMonthArray
-    }
-
-    fun monthYearFromDate(date: LocalDate?):String{
-        val formatter:DateTimeFormatter  = DateTimeFormatter.ofPattern("MMMM yyyy")
-        return date?.format(formatter) ?: String()
-    }
-
-    override fun onItemClick(position: Int, dayText: String?) {
-
-    }
-
 }
