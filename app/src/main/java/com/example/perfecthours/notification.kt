123257b808360afbf721e3923cf9d97e80887861
Nischal Baral <nischalbaral267@gmail.com>
2022-12-01 20:21:18 +0545

    History created

--- app/src/main/java/com/example/perfecthours/notification.kt
+++ app/src/main/java/com/example/perfecthours/notification.kt
@@ -6,16 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [notification.newInstance] factory method to
- * create an instance of this fragment.
- */
 class notification : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
